fb5db591eef81c1f7e61916352d7c7c2190f1f45
Prinston <tylerray@live.com>
2022-02-24 03:48:38 -0800

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Prinston'
+description 'Modular Economy Server'
+version '0.1'
+
+shared_script 'src/shared/shared.js'
+server_script 'src/server/server.js'
+client_script 'src/client/client.js'
